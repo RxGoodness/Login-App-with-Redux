a063c7e78ed26fc15c0b4879e69af277d52fadef
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-18 17:05:02 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Profile from './Profile';
 import Login from './Login';
+import ChangeColor from './ChangeColor';
 
 function App() {
   return (
     <div className="App">
       <Profile/>
       <Login/>
+      <ChangeColor/>
       Hello world!
     </div>
   );
